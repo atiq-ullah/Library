501bedf3eb26e7274bf04aefbb439d0d1442a260
Atiq Ullah <atiqullah72@gmail.com>
2024-06-21 13:35:31 -0700

    feat: add logger thread

---
+++ Logging/ConsoleHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Logging
+{
+    public partial class ConsoleHelper
+    {
+
+        [LibraryImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static partial bool AllocConsole();
+
+        [LibraryImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static partial bool FreeConsole();
+
+        [LibraryImport("kernel32.dll", SetLastError = true)]
+        public static partial IntPtr GetConsoleWindow();
+
+        [LibraryImport("kernel32.dll", SetLastError = true)]
+        public static partial IntPtr GetStdHandle(int nStdHandle);
+
+        [LibraryImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static partial bool SetStdHandle(int nStdHandle, IntPtr handle);
+
+        [LibraryImport("user32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static partial bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private const int SW_HIDE = 0;
+        private const int SW_SHOW = 5;
+        private const int STD_OUTPUT_HANDLE = -11;
+
+        public static void CreateNewConsole()
+        {
+            FreeConsole();
+            AllocConsole();
+
+            IntPtr consoleHandle = GetConsoleWindow();
+            ShowWindow(consoleHandle, SW_SHOW);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,33 @@
 ﻿using Library;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using Spectre.Console;
+using Library.Logging;
 using Spectre.Console.Cli;
+using System.Threading;
 
-var app = new CommandApp<LibraryCommand>();
-app.Run(args);
+Thread loggingThread = new Thread(() =>
+{
+    ConsoleHelper.CreateNewConsole();
+    using (var writer = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true })
+    {
+        Console.SetOut(writer);
+        Console.WriteLine("Logging to the new console");
+        while (true)
+        {
+            Console.WriteLine($"Log message at {DateTime.Now}");
+            Thread.Sleep(1000);
+        }
+    }
+});
+
+loggingThread.Start();
+using (var mainWriter = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true })
+{
+    Console.SetOut(mainWriter);
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine($"Main thread message {i}");
+        Thread.Sleep(2000); // Simulate work in the main thread
+    }
+}
+
+//  var app = new CommandApp<LibraryCommand>();
+//    app.Run(args);
